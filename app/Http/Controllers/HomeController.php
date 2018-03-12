466c3db2e62303a2def1e951a6f7d11782ca5ab2
stefferd <vandenberg.stef@gmail.com>
2018-03-12 14:21:12 +0100

    Added the monthly overview with labels for the current month and formatted the date with Carbon

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Project;
 use App\Registration;
-use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
@@ -26,9 +26,26 @@ class HomeController extends Controller
      */
     public function index()
     {
+	    $thisMonthRange = $this->getCurrentMonth();
+	    $currentMonthLabel = Carbon::now()->formatLocalized('%B');
     	$customers = DB::table('customers')->where('user_id', auth()->user()->id)->get();
     	$projects = Project::orderBy('customer_id')->with('customer')->where('user_id', auth()->user()->id)->get();
-    	$registrations = Registration::orderBy('workday', 'desc')->with(['project', 'customer'])->where('user_id', auth()->user()->id)->get();
-        return view('home', ['customers' => $customers, 'projects' => $projects, 'registrations' => $registrations]);
+    	$registrations = Registration::orderBy('workday', 'desc')->with(['project', 'customer'])->where('user_id', auth()->user()->id)->whereBetween('workday', $thisMonthRange)->get();
+        return view('home', [
+        	'customers' => $customers,
+	        'projects' => $projects,
+	        'registrations' => $registrations,
+	        'currentMonth' => $currentMonthLabel]
+        );
+    }
+
+    private function getCurrentMonth() {
+    	$endOfMonth = Carbon::now()->endOfMonth();
+    	$beginOfMonth = Carbon::now()->startOfMonth();
+
+    	return [
+    		$beginOfMonth,
+		    $endOfMonth
+	    ];
     }
 }
